7af63091f18792cc7bd62756ce7a0b1c66d7b280
morty9352 <garymorton@pm.me>
2021-05-20 21:26:39 -0500

    Fixed main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,20 +1,23 @@
-window.addEventListener('COMContentLoaded', (event) =>{
+window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
-})
+});
 
-const functionApiUrl = 'https://getresumecounter1175.azurewebsites.net/api/GetResumeCounter?code=FKiXe7Dy5Xz9GI4XCfRK1ialb/kdkTdHjSaL2rxCyPc02YfuMdfOYQ=='
-const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
+
+const localApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApi = 'https://getresumecounter1175.azurewebsites.net/api/GetResumeCounter?code=FKiXe7Dy5Xz9GI4XCfRK1ialb/kdkTdHjSaL2rxCyPc02YfuMdfOYQ==';
 
 const getVisitCount = () => {
     let count = 0;
-    fetch(functionApiUrl).then(response => {
+    fetch(localApi)
+    .then(response => {
         return response.json()
-    }).then(response =>{
+    })
+    .then(response => {
         console.log("Website called function API.");
         count = response.count;
         document.getElementById('counter').innerText = count;
-    }).catch(function(error){
+    }).catch(function(error) {
         console.log(error);
-    });
+      });
     return count;
 }
